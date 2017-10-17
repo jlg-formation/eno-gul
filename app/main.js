c24c9b9e35908db2b21a3d8689f13c5e3e9f2a15
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-17 16:24:33 +0200

    webpack

--- app/main.js
+++ app/main.js
@@ -1,13 +1,8 @@
-(function() {
-    'use strict';
+const app = angular.module('main', []);
 
-
-    const app = angular.module('main', []);
-
-    app.component('enoHeader', {
-        templateUrl: './tmpl/eno-header.html'
-    });
-    app.component('enoFooter', {
-        templateUrl: './tmpl/eno-footer.html'
-    });
-})();
+app.component('enoHeader', {
+    templateUrl: './tmpl/eno-header.html'
+});
+app.component('enoFooter', {
+    templateUrl: './tmpl/eno-footer.html'
+});
