6fb1c75db08d212aee6aa1b92deaf5bd8dc5b469
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-17 17:21:25 +0200

    html

--- app/main.js
+++ app/main.js
@@ -2,11 +2,14 @@ import 'angular';
 import 'angular/angular-csp.css';
 import './style.scss';
 
+import enoHeaderHtml from './tmpl/eno-header.html';
+import enoFooterHtml from './tmpl/eno-footer.html';
+
 const app = angular.module('main', []);
 
 app.component('enoHeader', {
-    templateUrl: './tmpl/eno-header.html'
+    template: enoHeaderHtml
 });
 app.component('enoFooter', {
-    templateUrl: './tmpl/eno-footer.html'
+    template: enoFooterHtml
 });
