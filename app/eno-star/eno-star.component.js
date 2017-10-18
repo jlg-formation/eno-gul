c924c150266178142515d94e9cbc12883f8d0fc3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-18 10:25:31 +0200

    databinding

--- app/eno-star/eno-star.component.js
+++ app/eno-star/eno-star.component.js
@@ -1,9 +1,25 @@
 export const enoStar = {
+    bindings: {
+        note: '='
+    },
     controller: function EnoStarCtrl($element) {
         'ngInject';
-        console.log('EnoStarCtrl', arguments);
-        const html = 'adsfasdfafsd';
-        $element.html(html);
+        
+
+        this.$onInit = () => {
+            console.log('EnoStarCtrl', arguments);
+            console.log('ctrl.note', this.note);
+            let html = '';
+            const note = this.note || 3;
+            for (let i = 0; i < note; i++) {
+                html += '<img src="eno-star/img/yellow_star.png">';
+            }
+            for (let i = note; i < 5; i++) {
+                html += '<img src="eno-star/img/white_star.png">';
+            }
+            $element.html(html);
+        };
+
     }
 };
 
